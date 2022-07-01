04d83418c5f31d57b7d85b32700fa6b642e740f8
Andreea Gardelean <zjac013@cim-ts-node-02.cc.rhul.local>
2022-07-01 09:32:51 +0100

    new feature and design alteration

--- conversion.py
+++ conversion.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def firstTemplate():
     return render_template('base.html')
+
+@app.route("/about")
+def aboutTemplate():
+    return render_template('about.html')
